9d31fb6762cf77c3049292d5dc41cb865b65a8dd
Miguel Inocente <miguelander30@gmail.com>
2026-09-05 12:59:56 -0500

    Creando el Fronted

---
+++ Quality-Oportunities-app/README.md
@@ -0,0 +1,21 @@
+Dirigirse a la ruta de la carpeta 
+
+```
+cd Quality-Oportunities-app/
+```
+
+Luego ejecutar los siguientes comandos
+
+```
+npm install
+```
+y
+
+```
+npm run dev
+```
+
+Se mostrare un url: http://localhost:5173/
+
+
+Requisitos: Tener instalado Node.js
---
+++ Quality-Oportunities-app/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+         <h1 class="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
